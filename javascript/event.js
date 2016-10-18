460361ce28da32a15449f8948760517998c7d1d6
yuehao.wang <yuehao.wang@aispeech.com>
2016-10-18 17:03:48 +0800

    调整判断结构

--- javascript/event.js
+++ javascript/event.js
@@ -14,18 +14,37 @@ function playerEvent(event) {
             break;
     }
 }
-
+//移动角色并做相应的操作
 function playerMove(x, y, status) {
-    if (!canMove(x, y)) {
+    var charaInfo = canMove(x, y);
+
+    if (charaInfo === false) {
         return;
     };
+
+    if (charaInfo.chara === "monster") {
+        if (charaInfo.move === true) {
+            layers.chara.removeChild(layers.chara.childList[charaInfo.index]);
+            senceData.character[charaInfo.index].show = false;
+            LEvent.removeEventListener(window, LKeyboardEvent.KEY_DOWN, playerEvent);
+            var timer = new LTimer(1000, 1);
+            timer.addEventListener(LTimerEvent.TIMER, function () {
+                LEvent.addEventListener(window, LKeyboardEvent.KEY_DOWN, playerEvent);
+            });
+            timer.start();
+        }
+        if (charaInfo.move === false) {
+            return;
+        }
+    }
+
     player.gotoAndPlay(status);
     globalData.playerInfo.position.x += x;
     globalData.playerInfo.position.y += y;
     player.x = globalData.playerInfo.position.x * imgCellWidth;
     player.y = globalData.playerInfo.position.y * imgCellHeight;
 }
-
+//判断是否可以移动
 function canMove(x, y) {
     var tempX = globalData.playerInfo.position.x + x,
         tempY = globalData.playerInfo.position.y + y;
@@ -46,7 +65,10 @@ function canMove(x, y) {
             var Chara = senceData.character[i];
             switch (Chara.type) {
                 case "npc":
-                    return false;
+                    return {
+                        chara: "npc",
+                        move: false
+                    };
                 case "monster":
                     return killMonster(j - 1); // j-1 因为j++ 才表示当前
             }
@@ -54,11 +76,11 @@ function canMove(x, y) {
     }
     return true;
 }
-
+//不能杀死的怪物 角色不能移动
 function killMonster(i) {
-    layers.chara.removeChild(layers.chara.childList[i]);
-    senceData.character[i].show = false;
-    LEvent.removeEventListener(window, LKeyboardEvent.KEY_DOWN, playerEvent);
-    // killAnimation();
-    return true;
+    return {
+        chara: "monster",
+        move: true,
+        index: i
+    };
 }
