7026d72fb0f346fdbe1c807a800258fe5c37c391
Yuehao Wang <352209095@qq.com>
2017-07-04 01:10:28 +0800

    重新设计了代码 优化了配置流程 之后会已这个思路开发 目前实现上下楼 不能开门和拾取钥匙

--- javascript/event.js
+++ javascript/event.js
@@ -1,51 +1,66 @@
 //移动角色并做相应的操作
-var playerMove = function (x, y, status, player, playPosition) {
-    var charaInfo = canMove(x, y, playPosition);
-
-    if (charaInfo === false) {
+var playerMove = function (x, y, status) {
+    var moveInfo = canMove(x, y);
+    console.log(moveInfo)
+    if (moveInfo === false) {
         return;
     }
-
-    if (charaInfo.chara === "monster") {
-        if (charaInfo.move === true) {
-            layers.chara.removeChild(layers.chara.childList[charaInfo.index]);
-            senceData.character[charaInfo.index].show = false;
-            LEvent.removeEventListener(window, LKeyboardEvent.KEY_DOWN, playerEvent);
-            var timer = new LTimer(1000, 1);
-            timer.addEventListener(LTimerEvent.TIMER, function () {
-                LEvent.addEventListener(window, LKeyboardEvent.KEY_DOWN, playerEvent);
-            });
-            timer.start();
-        }
-        if (charaInfo.move === false) {
-            return;
+    if (moveInfo.type === 'goods') {
+        switch (moveInfo.goodsType) {
+            case 'upFloor':
+                globalData.floor++;
+                drawInit();
+                return; /*不执行之后的运动操作*/
+                break;
+            case 'downFloor':
+                globalData.floor--;
+                drawInit();
+                return; /*不执行之后的运动操作*/
+                break;
         }
     }
+    // if (charaInfo.chara === "monster") {
+    //     if (charaInfo.move === true) {
+    //         layers.chara.removeChild(layers.chara.childList[charaInfo.index]);
+    //         senceData.character[charaInfo.index].show = false;
+    //         LEvent.removeEventListener(window, LKeyboardEvent.KEY_DOWN, playerEvent);
+    //         var timer = new LTimer(1000, 1);
+    //         timer.addEventListener(LTimerEvent.TIMER, function () {
+    //             LEvent.addEventListener(window, LKeyboardEvent.KEY_DOWN, playerEvent);
+    //         });
+    //         timer.start();
+    //     }
+    //     if (charaInfo.move === false) {
+    //         return;
+    //     }
+    // }
     // npc的话可以进行转向但是不能移动
     player.gotoAndPlay(status);
     globalData.playerInfo.status = status
-    if (charaInfo.chara === 'floor') {
-        globalData.floor += charaInfo.floorType
-        console.log(globalData.floor)
-        senceData = globalData.data[globalData.floor]
-        gameBegin()
-        return /*直接返回 不让英雄移动*/
-    }
+    // if (charaInfo.chara === 'floor') {
+    //     globalData.floor += charaInfo.floorType
+    //     console.log(globalData.floor)
+    //     senceData = globalData.data[globalData.floor]
+    //     gameBegin()
+    //     return
+    //     /*直接返回 不让英雄移动*/
+    // }
 
-    if (charaInfo.chara === 'npc') {
-        if (charaInfo.move === false) {
-            return
-        }
-    }
-    playPosition.x += x;
-    playPosition.y += y;
-    player.x = playPosition.x * globalData.size;
-    player.y = playPosition.y * globalData.size;
+    // if (charaInfo.chara === 'npc') {
+    //     if (charaInfo.move === false) {
+    //         return
+    //     }
+    // }
+    senceData.playerPosition.x += x;
+    senceData.playerPosition.y += y;
+    player.x = senceData.playerPosition.x * globalData.size;
+    player.y = senceData.playerPosition.y * globalData.size;
 }
 //判断是否可以移动
-function canMove(x, y, playPosition) {
-    var tempX = playPosition.x + x,
-        tempY = playPosition.y + y;
+function canMove(x, y) {
+    var tempX = senceData.playerPosition.x + x,
+        tempY = senceData.playerPosition.y + y;
+    var type = '';
     //限制行动区域
     if (tempX < 0 || tempY < 0 || tempY >= globalData.mapRow || tempX >= globalData.mapCol) {
         return false;
@@ -53,6 +68,29 @@ function canMove(x, y, playPosition) {
     //撞墙检测
     if (senceData.map[tempY][tempX] !== 0) {
         return false;
+    }
+    // 碰撞物品
+    if (senceData.goods[tempY][tempX] !== 0) {
+        type = getType(senceData.goods[tempY][tempX])
+        switch (type) {
+            case 'upFloor':
+                return {
+                    type: 'goods',
+                    goodsType: 'upFloor'
+                }
+                break;
+            case 'downFloor':
+                return {
+                    type: 'goods',
+                    goodsType: 'downFloor'
+                }
+                break;
+        }
+        return false
+    }
+    //碰撞人物
+    if (senceData.chara[tempY][tempX] !== 0) {
+        return false
     }
     //碰撞npc
     // for (var i = 0, j = 0; i < senceData.character.length; i++) {
@@ -83,4 +121,7 @@ function killMonster(i) {
         move: true,
         index: i
     };
+}
+function getType(str) {
+    return str.match(/[a-zA-Z]/g).join('')
 }
--- javascript/main.js
+++ javascript/main.js
@@ -111,11 +111,10 @@ function gameInit(result) {
     removeChild(loadingLayer);
     loadingLayer = null;
     imgList = result;
-    senceData = globalData.data[globalData.floor];
     //游戏层显示初始化
     layerInit();
     //游戏场景载入
-    gameBegin();
+    drawInit();
 
     LEvent.addEventListener(LGlobal.window, LKeyboardEvent.KEY_DOWN, playerEvent);
 }
@@ -123,22 +122,23 @@ function gameInit(result) {
 function playerEvent(event) {
     switch (event.keyCode) {
         case 37:
-            playerMove(-1, 0, "ArrowLeft", player, globalData.playerInfo.position, senceData);
+            playerMove(-1, 0, "ArrowLeft");
             break;
         case 38:
-            playerMove(0, -1, "ArrowUp", player, globalData.playerInfo.position, senceData);
+            playerMove(0, -1, "ArrowUp");
             break;
         case 39:
-            playerMove(1, 0, "ArrowRight", player, globalData.playerInfo.position, senceData);
+            playerMove(1, 0, "ArrowRight");
             break;
         case 40:
-            playerMove(0, 1, "ArrowDown", player, globalData.playerInfo.position, senceData);
+            playerMove(0, 1, "ArrowDown");
             break;
     }
 }
 
 
-function gameBegin() {
+function drawInit() {
+    senceData = globalData.data[globalData.floor];
     // 页面初始化
     layers.chara.removeAllChild();
     layers.mapview.removeAllChild();
@@ -146,11 +146,6 @@ function gameBegin() {
     addChara();
     addGoods();
     addPlayer();
-    // addDoor();
-    // addMonster();
-    // for (var i = 0; i < senceData.character.length; i++) {
-    //     addChara(senceData.character[i]);
-    // }
 }
 
 //添加地图
@@ -193,6 +188,7 @@ function addChara() {
 
     for (var i = 0; i < globalData.mapRow; i++) {
         for (var j = 0; j < globalData.mapCol; j++) {
+            console.log(senceData.chara[i][j])
             index = senceData.chara[i][j];
             console.log(index)
             if (index !== 0) {
@@ -202,28 +198,6 @@ function addChara() {
                 bitMap.setLabel("npc", configChara[index].y, 0, 1, true);
                 bitMap.speed = 5;
                 bitMap.gotoAndPlay("npc");
-                bitMap.x = j * globalData.size;
-                bitMap.y = i * globalData.size;
-                layers.chara.addChild(bitMap);
-            }
-        }
-    }
-}
-function addMonster() {
-    var bitMapData = null,
-        bitMap = null;
-    var index;
-
-    for (var i = 0; i < globalData.mapRow; i++) {
-        for (var j = 0; j < globalData.mapCol; j++) {
-            index = senceData.monster[i][j];
-            if (index !== 0) {
-                bitMapData = new LBitmapData(imgList[configMonster[index].img]);
-                mapImagesArray = LGlobal.divideCoordinate(bitMapData.width, bitMapData.height, 4, 4);
-                bitMap = new LAnimationTimeline(bitMapData, mapImagesArray);
-                bitMap.setLabel("monster", configMonster[index].y, 0, 1, true);
-                bitMap.speed = 5;
-                bitMap.gotoAndPlay("monster");
                 bitMap.x = j * globalData.size;
                 bitMap.y = i * globalData.size;
                 layers.chara.addChild(bitMap);
@@ -265,71 +239,6 @@ function addPlayer() {
         chara.y = senceData.playerPosition.y * globalData.size;
         layers.chara.addChild(chara);
 }
-function addDoor() {
-    var bitMapData = null,
-        bitMap = null;
-    var index;
-
-    for (var i = 0; i < globalData.mapRow; i++) {
-        for (var j = 0; j < globalData.mapCol; j++) {
-            index = senceData.door[i][j];
-            if (index !== 0) {
-                bitMapDataCell = new LBitmapData(imgList[configDoor[index].img], configDoor[index].x * globalData.size, configDoor[index].y * globalData.size, globalData.size, globalData.size);
-                bitMap = new LBitmap(bitMapDataCell);
-                bitMap.x = j * globalData.size;
-                bitMap.y = i * globalData.size;
-                layers.chara.addChild(bitMap);
-            }
-        }
-    }
-}
-//添加人物
-// function addChara(character) {
-//     // 如果人物死掉了 怪物属性
-//     if (character.show === false) {
-//         return false;
-//     }
-//     var bitMapData = new LBitmapData(imgList[character.name]);
-//     var charaWidth = bitMapData.width,
-//         charaHeight = bitMapData.height;
-//     var listChara = LGlobal.divideCoordinate(charaWidth, charaHeight, 4, 4);
-//     var chara = new LAnimationTimeline(bitMapData, listChara);
-//     if (character.type === "hero") {
-//         player = chara;
-//         player.setLabel("ArrowLeft", 2, 0, 1, true);
-//         player.setLabel("ArrowUp", 3, 0, 1, true);
-//         player.setLabel("ArrowRight", 1, 0, 1, true);
-//         player.setLabel("ArrowDown", 0, 0, 1, true);
-//         // 给英雄一个独立的速度 为了画面协调
-//         player.speed = 10;
-//         player.gotoAndPlay(globalData.playerInfo.status);
-//         globalData.playerInfo.position = character.position;
-//     }
-//     if (character.type === "monster") {
-//         chara.setLabel("monster", character.monsterType, 0, 1, true);
-//         chara.gotoAndPlay("monster");
-//     }
-//     if (character.type === "npc") {
-//         chara.setLabel("npc", character.npcType, 0, 1, true);
-//         chara.gotoAndPlay("npc");
-//     }
-//     chara.speed = 3;
-//     if (character.type === "floor") {
-//         var bitMapDataCell = new LBitmapData(imgList[character.name], 0, 0, globalData.size, globalData.size);
-//         chara = new LBitmap(bitMapDataCell);
-//     }
-//     if (character.type === "key") {
-//         var bitMapDataCell = new LBitmapData(imgList[character.name], character.keyType.x * globalData.size, character.keyType.y * globalData.size, globalData.size, globalData.size);
-//         chara = new LBitmap(bitMapDataCell);
-//     }
-//     if (character.type === "door") {
-//         var bitMapDataCell = new LBitmapData(imgList[character.name], character.doorType * globalData.size, 0, globalData.size, globalData.size);
-//         chara = new LBitmap(bitMapDataCell);
-//     }
-//     chara.x = character.position.x * globalData.size;
-//     chara.y = character.position.y * globalData.size;
-//     layers.chara.addChild(chara);
-// };
 
 function layerInit() {
     layers.back = new LSprite();
